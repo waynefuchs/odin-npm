8c90cbc5f724b5e9cae7c45c5c01c760e02cb51e
Wayne Fuchs <wayne.fuchs@icloud.com>
2022-08-30 23:57:47 -0500

    Prettier

--- dropdown/index.js
+++ dropdown/index.js
@@ -1,21 +1,22 @@
-function clearClass(classText, except=null) {
-  const allClicked = Array.from(document.querySelectorAll(`.${classText}`)).filter(element => element !== except);
-  allClicked?.forEach(clicked => clicked.classList.remove(classText));
+function clearClass(classText, except = null) {
+  const allClicked = Array.from(
+    document.querySelectorAll(`.${classText}`)
+  ).filter((element) => element !== except);
+  allClicked?.forEach((clicked) => clicked.classList.remove(classText));
 }
 
 function addEventMouseEnter(li) {
-  li.addEventListener('mouseenter', (event) => {
-    clearClass('clicked', event.target);
+  li.addEventListener("mouseenter", (event) => {
+    clearClass("clicked", event.target);
     event.stopPropagation();
   });
 }
 
 function addEventMouseClick(li) {
   li.addEventListener("click", (event) => {
-    const isClicked = event.target.classList.contains('clicked');
-    clearClass('clicked');
-    if (!isClicked)
-      event.target.classList.add('clicked');
+    const isClicked = event.target.classList.contains("clicked");
+    clearClass("clicked");
+    if (!isClicked) event.target.classList.add("clicked");
   });
 }
 
@@ -28,11 +29,12 @@ function dropdown(dropdowns) {
 
 const attach = (selector) => {
   const elements = Array.from(document.querySelectorAll(selector));
-  if(elements.length <= 0) return;
+  if (elements.length <= 0) return;
   dropdown(elements);
-}
+};
 
 (function () {
-  'use strict';
+  "use strict";
   exports.attach = attach;
-}());
+  exports.clearClass = clearClass;
+})();
